9b0cb69503b4f7591489b35cc1fb3478446dbeae
WingsHell <kevin.alvespinto@gmail.com>
2019-07-12 14:38:45 +0200

    Add components to Home module

---
+++ src/app/public/home/home-feature/home-feature.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'al-home-feature',
+  templateUrl: './home-feature.component.html',
+  styles: []
+})
+export class HomeFeatureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
